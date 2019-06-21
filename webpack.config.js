44ce3404fc546e2997eb6aa5312f2e8d4ce6c1d9
meetqy <37372842@qq.com>
2019-06-21 18:11:12 +0800

    webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,96 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const webpack = require("webpack");
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const webpack = require("webpack");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    main: "./src/index.js"
-  },
+// module.exports = {
+//   mode: "development",
+//   entry: {
+//     main: "./src/index.js"
+//   },
 
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    // compress: true,
-    hot: true,
-    hotOnly: true
-  },
+//   devServer: {
+//     contentBase: path.join(__dirname, "dist"),
+//     compress: true,
+//     hot: true,
+//     hotOnly: true
+//   },
 
-  devtool: "cheap-source-map",
+//   devtool: "cheap-source-map",
 
-  output: {
-    filename: "[name]_[hash].js",
-    path: path.resolve(__dirname, "dist")
-  },
+//   output: {
+//     filename: "[name]_[hash].js",
+//     path: path.resolve(__dirname, "dist")
+//   },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "meetqyEditor",
-      template: "./src/index.html"
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+//   plugins: [
+//     new CleanWebpackPlugin(),
+//     new HtmlWebpackPlugin({
+//       title: "meetqyEditor",
+//       template: "./src/index.html"
+//     }),
+//     new webpack.HotModuleReplacementPlugin(),
+//     new ExtractTextPlugin("styles/main.css")
+//   ],
 
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            outputPath: "images"
-          }
-        }
-      },
-      {
-        test: /\.(eot|ttf|svg)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            outputPath: "fonts"
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 2
-            },
-          },
-          "postcss-loader",
-          "sass-loader"
-        ]
-      },
-      { 
-        test: /\.js$/, 
-        exclude: /node_modules/, 
-        loader: "babel-loader"
-      }
-    ]
-  }
-};
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(png|jpe?g|gif)$/,
+//         use: {
+//           loader: "file-loader",
+//           options: {
+//             outputPath: "images"
+//           }
+//         }
+//       },
+//       {
+//         test: /\.(eot|ttf|svg)$/,
+//         use: {
+//           loader: "file-loader",
+//           options: {
+//             outputPath: "fonts"
+//           }
+//         }
+//       },
+//       {
+//         test: /\.scss$/,
+//         // development
+//         // use: ExtractTextPlugin.extract({
+//         //   fallback: "style-loader",
+//         //   use: [
+//         //     {
+//         //       loader: "css-loader",
+//         //       options: {
+//         //         modules: true,
+//         //         importLoaders: 2,
+//         //       }
+//         //     },
+//         //     "postcss-loader",
+//         //     "sass-loader"
+//         //   ]
+//         // })
+
+//         // production
+//         use: [
+//           'style-loader',
+//           {
+//             loader: 'css-loader',
+//             options: {
+//               modules: true,
+//               importLoaders: 2
+//             },
+//           },
+//           'postcss-loader',
+//           'sass-loader'
+//         ]
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: "babel-loader"
+//       }
+//     ]
+//   }
+// };
