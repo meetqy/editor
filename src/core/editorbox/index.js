580af9b7f66cf32d7de82819c395686caed29480
meetqy <37372842@qq.com>
2019-06-22 20:26:24 +0800

    将选中的文本转换

--- src/core/editorbox/index.js
+++ src/core/editorbox/index.js
@@ -1,22 +1,47 @@
+import ReplaceSelect from './ReplaceSelect';
+
 class EditorBox {
   constructor(MEditor) {
     this.MEditor = MEditor;
+    this._Rs = ReplaceSelect;
 
     this._el = this._init();
+    this.listenEl();
   }
 
   _init() {
     let editorBox = document.createElement('div');
     editorBox.classList.add('m-e-box');
-    // editorBox.setAttribute('contenteditable', true);
     let container = document.createElement('div');
     container.classList.add('m-e-container');
     container.setAttribute('contenteditable', true);
+    container.innerHTML = '<p><br/></p>';
     editorBox.appendChild(container);
 
     this.MEditor._el.appendChild(editorBox);
 
     return container;
+  }
+
+  // 对el进行事件监听
+  listenEl() {
+    let { _el } = this;
+    _el.addEventListener('keydown', e => {
+      if(!_el.innerHTML) {
+        _el.innerHTML += '<p><br/></p>'
+      }
+    })  
+
+    _el.addEventListener('mousedown', e => {
+
+    })
+
+    _el.addEventListener('mouseup', e => {
+      let select = getSelection();
+      if(!select.toString()) return;
+
+      this._Rd(select).h1();
+    })
   }
 }
 
